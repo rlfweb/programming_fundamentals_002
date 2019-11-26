ae791a5bcc926c21a469e5a9ae3d562d8c0acf2e
Rebecca French <rlfweb8@gmail.com>
2019-11-26 13:55:16 +0000

    Working through rest of exercises in Programming Fundamentals 002 - final one still not passing

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -23,24 +23,84 @@ const catalogue = [
 ];
 
 function countBooks() {
-  // Your code here
+  return catalogue.length;
 }
 
 function checkBook(book) {
   // Your code here
+  for (let i = 0; i < catalogue.length; i++) { //check through all of the books in the array
+    let bookToCheck = catalogue[i];
+
+    if (bookToCheck === book) {
+      return true;
+    }
+  }
+  return false;
 }
 
+
 function countBooksByFirstLetter(letter) {
-  // Your code here
+  // This tells computer that count begins at zero
+  let count = 0; 
+  // This tells computer what your loop is - to start looping at index point zero, to keep going until the end of the array and to go through them one at a time
+  for (let i = 0; i < catalogue.length; i++) {
+  // This tells computer to go through the catalogue array and each time it finds a title that starts with the letter (in this case W is being tested for by the test.js)
+    if (catalogue[i].startsWith(letter)){
+  // This tells computer to add one to the count variable each time it finds a title beginning with W
+      count++
+    }
+  }
+  // And then we are asking it to return 'count', which has now changed from zero because the computer has added 1 to it each time it found a title starting with W
+  return count;
 }
 
 function countBooksByKeyword(keyword) {
-  // Your code here
+  // This would be similar to the function above, but instead of ...ByFirstLetter it would be ...ByKeyword
+// So I'll start off by copying the one above and replacing 'letter' with 'keyword'
+// That didn't pass the test the first time, but then I noticed there was .startsWith in there
+// I looked in Mozilla dev reference for something else. .contains didn't seem to exist. But .match works 
+// I did have to change the case of the words being tested to Title case i.e. Assassin not assassin. 
+// So I guess .match has to match the data exactly, which may not be entirely what is wanted as users may not match case
+
+// This tells computer that count begins at zero
+let count = 0; 
+// This tells computer what your loop is - to start looping at index point zero, to keep going until the end of the array and to go through them one at a time
+for (let i = 0; i < catalogue.length; i++) {
+// This tells computer to go through the catalogue array and each time it finds a book title that contains this keyword (in this case assassin is being tested for by the test.js)
+    if (catalogue[i].match(keyword)){
+// This tells computer to add one to the count variable each time it finds a title containing the word "assassin"
+    count++
+  }
 }
+// And then we are asking it to return 'count', which has now changed from zero because the computer has added 1 to it each time it found a title containing the keyword "assassin"
+return count;
+}
+// I added another test that looked for a number - first I did "2666" so it was still treating the number like a string. That passed. 
+// Then I took out the speech marks and so the number was more of a number. That still passes. 
+// What if someone calls the function with a number? [1, 2, 3] - this passes
+// Or a boolean? - this brings up an error. 
+
 
 function getBooksByAuthor(author) {
-  // Your code here
+  // Writing it out in pseudocode first ...
+
+  // I need to return a new array of all the books by a particular author. 
+  // So I need computer to go through the whole catalogue array and not stop when it finds the first one. 
+
+  // So ...
+
+// check through the catalogue array from beginning to end, one at a time
+for (let i = 0; i < catalogue.length; i++) {
+// when you find the string that contains the author name, keep hold of it
+if (catalogue[i].filter(author)){
+// when the string doesn't contain author name, discard it
+// at the end, return a new array of all the strings that contain our particular author
 }
+return [""];
+}
+};
+//I'm going round in circles with this one. I'll move onto doing exercises from 003 and then maybe return to this if I have time on Wednesday or Thursday. 
+
 
 module.exports = {
   countBooks,
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -6,4 +6,67 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooks()).toBe(20);
     });
   });
+
+  describe("catalogueService.checkBook", () => {
+    test("returns true if the book exists in the list", () => {
+      expect(catalogueService.checkBook("Dracula by Bram Stoker")).toBe(true);
+    });
+
+    test("returns false if the book does not exist in the list", () => {
+      expect(catalogueService.checkBook("Moths by Pamela Mothman")).toBe(false);
+    });
+  });
+
+});
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+    });
+  });
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("T")).toBe(5);
+    });
+  
+    test("returns 0 if no books begin with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("X")).toBe(0);
+    });
+  });
+
+
+
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns the number of books that contain the keyword", () => {
+      expect(catalogueService.countBooksByKeyword("Assassin")).toBe(3);
+    });
+  
+
+    test("returns the number of books that contain the keyword", () => {
+      expect(catalogueService.countBooksByKeyword("Normal")).toBe(2);
+    });
+
+      test("returns the number of books that contain the keyword", () => {
+        expect(catalogueService.countBooksByKeyword("pineapple")).toBe(0);
+      });
+
+      test("returns the number of books that contain the keyword", () => {
+        expect(catalogueService.countBooksByKeyword(2666)).toBe(1);
+      });
+
+      test("returns the number of books that contain the keyword", () => {
+        expect(catalogueService.countBooksByKeyword([1, 2, 3])).toBe(0);
+      });
+  
+  });
+
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns a list of books by a particular author", () => {
+  expect(catalogueService.getBooksByAuthor("Charles Dickens")).toEqual([
+    "A Tale of Two Cities by Charles Dickens",
+    "Oliver Twist by Charles Dickens",
+    "Great Expectations by Charles Dickens"
+  ]);
+});
 });
